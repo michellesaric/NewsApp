0621cef3de263a3ef504bb602a7dbf57f4901e5e
„michellesaric“ <msaric03@fesb.hr>
2023-04-24 19:58:50 +0200

    Added modal for navbar and added open and close function

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Categories from "../../pages/home/categories/Categories";
+import ExitIcon from "../exitIcon/ExitIcon";
+import SearchBar from "../search_bar/SearchBar";
+
+interface Props {
+  setIsModalOpen: (value: boolean) => void;
+}
+
+function Modal({ setIsModalOpen }: Props) {
+  return (
+    <div className="modal">
+      <div className="modal__exit-icon-wrapper">
+        <div onClick={() => setIsModalOpen(false)}>
+          <ExitIcon />
+        </div>
+      </div>
+      <div className="modal__logo-wrapper">
+        <h1 className="modal_wrapper__logo modal_logo">
+          <span className="modal_wrapper__logo--red">My</span>News
+        </h1>
+      </div>
+      <SearchBar />
+      <Categories />
+    </div>
+  );
+}
+
+export default Modal;
--- src/pages/home/navbar/Navbar.tsx
+++ src/pages/home/navbar/Navbar.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Hamburger from "../../../components/hamburger/Hamburger";
 import SearchBar from "../../../components/search_bar/SearchBar";
+import Modal from "../../../components/modal/Modal";
 
 function Navbar() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   return (
-    <div className="Nav">
-      <div className="Nav_wrapper">
-        <h1 className="Nav_wrapper__logo">
-          <span className="Nav_wrapper__logo--red">My</span>News
-        </h1>
-        <Hamburger />
+    <>
+      <div className="nav">
+        <div className="nav_wrapper">
+          <h1 className="nav_wrapper__logo">
+            <span className="nav_wrapper__logo--red">My</span>News
+          </h1>
+          <div onClick={() => setIsModalOpen(true)}>
+            <Hamburger />
+          </div>
+        </div>
+        <SearchBar />
       </div>
-      <SearchBar />
-    </div>
+      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
+    </>
   );
 }
 
