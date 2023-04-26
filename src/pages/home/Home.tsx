c1c18c4a2fce25af0672745dd3eba3a8364e0dd8
„michellesaric“ <msaric03@fesb.hr>
2023-04-26 15:07:07 +0200

    Added new variable and buttons for Featured and Latest news on Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./navbar/Navbar";
+import News from "./news/News";
+import LatestNews from "./latest_news/LatestNews";
 
 function Home() {
+  const [isFeatured, setIsFeatured] = useState<boolean>(true);
+
   return (
     <div className="wrapper">
       <Navbar />
+      <div className="button_wrapper">
+        <button
+          onClick={() => setIsFeatured(true)}
+          className={
+            "btn " +
+            (isFeatured ? "button_wrapper-red" : "button_wrapper-regular")
+          }
+        >
+          Featured
+        </button>
+        <button
+          onClick={() => setIsFeatured(false)}
+          className={
+            "btn " +
+            (!isFeatured ? "button_wrapper-red" : "button_wrapper-regular")
+          }
+        >
+          Latest
+        </button>
+      </div>
+      {isFeatured ? <News /> : <LatestNews />}
     </div>
   );
 }
