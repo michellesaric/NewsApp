fd0dbe355563c380297395f0ed7100965083f995
„michellesaric“ <msaric03@fesb.hr>
2023-05-01 23:05:13 +0200

    Added UI for Latest News

--- src/components/latest_news_card/LatestNewsCard.tsx
+++ src/components/latest_news_card/LatestNewsCard.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function LatestNewsCard() {
-  return <div>LatestNewsCard</div>;
+  return (
+    <div className="latest-news-card">
+      <h4 className="latest-news-card__time">14:30</h4>
+      <p className="latest-news-card__title">
+        Maintain Your Pc S Perfomance With Pc Programs
+      </p>
+    </div>
+  );
 }
 
 export default LatestNewsCard;
--- src/pages/home/latest_news/LatestNews.tsx
+++ src/pages/home/latest_news/LatestNews.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import LatestNewsCard from "../../../components/latest_news_card/LatestNewsCard";
+import RightArrow from "../../../components/icons/right_arrow/RightArrow";
 
 function LatestNews() {
-  return <div>LatestNews</div>;
+  return (
+    <div className="latest-news">
+      <div className="latest-news__header">
+        <div className="latest-news__header-circle"></div>
+        <h2 className="latest-news__header-title">Latest news</h2>
+      </div>
+      <div className="latest-news__list">
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+        <LatestNewsCard />
+      </div>
+      <div className="latest-news__footer">
+        <p className="latest-news__footer-text">See all news</p>
+        <RightArrow />
+      </div>
+    </div>
+  );
 }
 
 export default LatestNews;
